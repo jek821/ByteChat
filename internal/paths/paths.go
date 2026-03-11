d7ec5b7c8c038d6190d7d98de3a3d25a5d975b09
Jemanuel <jacobkriss2001@gmail.com>
2026-03-11 10:49:26 -0400

    finished paths.go starter functions and added logger.go for logging

--- internal/paths/paths.go
+++ internal/paths/paths.go
@@ -1,5 +1,59 @@
 package paths
 
-func ServerRuntimeDir() (string, error) {
+import (
+	"os"
+	"path/filepath"
+)
 
+const (
+	dot  = ".gochat"
+	serv = "server"
+	cli  = "client"
+	db   = "gochat.db"
+)
+
+func RootDir() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	rootDir := filepath.Join(home, dot)
+	if err := os.MkdirAll(rootDir, 0700); err != nil {
+		return "", err
+	}
+	return rootDir, nil
+}
+
+func ServerDir() (string, error) {
+	root, err := RootDir()
+	if err != nil {
+		return "", err
+	}
+	serverDir := filepath.Join(root, serv)
+	if err := os.MkdirAll(serverDir, 0700); err != nil {
+		return "", err
+	}
+
+	return serverDir, nil
+}
+
+func ClientDir() (string, error) {
+	root, err := RootDir()
+	if err != nil {
+		return "", err
+	}
+	clientDir := filepath.Join(root, cli)
+	if err := os.MkdirAll(clientDir, 0700); err != nil {
+		return "", err
+	}
+	return clientDir, nil
+}
+func DBPath() (string, error) {
+	root, err := ServerDir()
+	if err != nil {
+		return "", err
+	}
+
+	dbPath := filepath.Join(root, db)
+	return dbPath, nil
 }
