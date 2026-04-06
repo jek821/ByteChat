714c2de2ae4a533b855664d9864914883053bda1
Jemanuel <jacobkriss2001@gmail.com>
2026-04-06 15:56:07 -0400

    cleanup

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,14 +1,4 @@
 package store
 
-import (
-	"context"
-
-	"GoChat/pkg/types"
-)
-
 type Store interface {
-	Close() error
-
-	CreateUser(ctx context.Context, user *types.User) (int64, error)
-	GetUserByUsername(ctx context.Context, username string) (*types.User, error)
 }
